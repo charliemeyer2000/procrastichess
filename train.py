c34f7c261543f9cfdaa7708370b612f02002f8ee
Charlie Meyer <charlie.g.meyer@icloud.com>
2023-11-26 17:10:55 -0500

    this is where i have no idea what i'm doing

---
+++ train.py
@@ -0,0 +1,15 @@
+#!/Users/charlie/miniconda3/bin/python3
+from torch.utils.data import Dataset, DataLoader
+from CSVDataset import CSVDatasetimport 
+import numpy as np
+import torch
+
+
+class ChessValueFuncNetwork(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+
+    def forward(self, x):
+        pass   
+
